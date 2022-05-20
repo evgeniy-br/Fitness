9dc240b804027a953b7b9e04dcf05cd60f6938ce
evgeniy <br4tkov@yandex.com>
2022-05-20 14:05:54 +0300

    Вёрстка шапки и блока "Тренажёры"

---
+++ source/js/modules/video.js
@@ -0,0 +1,9 @@
+const video = document.querySelector('.about__video-container');
+
+video.addEventListener('click', () => {
+  if (video.classList.contains('playing')) {
+    return
+  }
+  video.classList.add('playing');
+  video.insertAdjacentHTML('afterbegin', '<iframe src="https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+});
