fe247c349d90f47ee6d603627e336903b510a7ec
evgeniy <br4tkov@yandex.com>
2022-06-02 00:18:43 +0300

    Добавляет фокус на кнопке запуска видео

--- source/js/modules/video.js
+++ source/js/modules/video.js
@@ -1,9 +1,11 @@
 const video = document.querySelector('.about__video-container');
+const videoButton = document.querySelector('.about__video-button');
 
-video.addEventListener('click', () => {
+videoButton.addEventListener('click', () => {
   if (video.classList.contains('playing')) {
     return;
   }
+  videoButton.remove();
   video.classList.add('playing');
   video.insertAdjacentHTML('afterbegin', '<iframe src="https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
 });
